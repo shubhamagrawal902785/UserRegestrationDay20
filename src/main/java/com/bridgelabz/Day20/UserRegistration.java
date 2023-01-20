7af69e7a974e591b25314b1483b84158d8853a9f
shubhamagrawal902785 <shubhamag937@gmail.com>
2023-01-20 20:44:25 +0530

    Added: one special char letter

--- src/main/java/com/bridgelabz/Day20/UserRegistration.java
+++ src/main/java/com/bridgelabz/Day20/UserRegistration.java
@@ -22,7 +22,7 @@ public class UserRegistration {
         }
 
         static boolean Phone(String str4){
-                boolean b4 = Pattern.compile("^(91)[-.+]{1}[1-9]{1}[0-9]{9}$").matcher(str4).matches();
+                boolean b5 = Pattern.compile("^[0-9]{1,}[!@#$%^&]{1,}[A-z]{1,}[a-zA-Z0-9]{5,}$").matcher(str5).matches();
                 return b4;
         }
         static boolean Password(String str5){
